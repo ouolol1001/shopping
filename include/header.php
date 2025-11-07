574c3f5a61e10ad7b28e24218c0df91f95b732a0
jun hong <khorjunhong2020@gmail.com>
2025-11-07 09:08:11 +0800

    fcommit

---
+++ include/header.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../css/style.css">
+<header>
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="product.php">Products</a>
+    </nav>
+</header>
