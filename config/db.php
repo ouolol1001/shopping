d53e301e3b9a445969fcd8c1eaf9c9afe38d1b10
jun hong <khorjunhong2020@gmail.com>
2025-11-11 09:17:07 +0800

    df

--- config/db.php
+++ config/db.php
@@ -1,16 +1,14 @@
 <?php
-
 session_start();
 
-$server = "localhost";
-$pass = "1001ouo";
-$user = "root";
-$db = "shopping";
+$server = "localhost";   // 或 localhost:3306
+$user = "root";          // MySQL 用户名
+$pass = "1001ouo";              // 没有密码时留空
+$db = "shopping";        // 数据库名
 
-$conn = new mysqli($server,$pass,$user,$db);
-if($conn->connect_error){
-    die("connect error");
+$conn = new mysqli($server, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
-
-
 ?>
